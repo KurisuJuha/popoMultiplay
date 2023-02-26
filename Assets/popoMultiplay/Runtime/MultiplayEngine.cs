ed6e74175f44165f30ddd73eadd3b4bb83067e9a
KYapp <kukiyomenaisapp@gmail.com>
2023-02-26 22:51:51 +0900

    multiplayengineにイベントハンドラーを追加

--- Assets/popoMultiplay/Runtime/MultiplayEngine.cs
+++ Assets/popoMultiplay/Runtime/MultiplayEngine.cs
@@ -8,6 +8,9 @@ namespace JuhaKurisu.PopoTools.Multiplay
     public class MultiplayEngine
     {
         private readonly Dictionary<ClientID, MultiplayClient> clients = new();
+        private readonly TickEventHandler OnTick;
+        private readonly ConnectedEventHandler OnConnected;
+        private readonly ClosedEventHandler OnClosed;
         private int playerCount;
     }
 }
