485e32f0fc4cfcf34e88cfb34616297ca9e0cf78
KYapp <kukiyomenaisapp@gmail.com>
2023-02-26 22:49:46 +0900

    multiplayinputにコンストラクタ追加

--- Assets/popoMultiplay/Runtime/MultiplayInput.cs
+++ Assets/popoMultiplay/Runtime/MultiplayInput.cs
@@ -1,7 +1,17 @@
+using System.Linq;
+using JuhaKurisu.PopoTools.ByteSerializer;
+
 namespace JuhaKurisu.PopoTools.Multiplay
 {
     public struct MultiplayInput
     {
         public readonly bool testButton;
+
+        internal MultiplayInput(byte[] bytes)
+        {
+            DataReader reader = new(bytes);
+
+            testButton = reader.ReadBoolean();
+        }
     }
 }
