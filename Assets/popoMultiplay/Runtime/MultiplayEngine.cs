9c4d819cb2151012b90d875e7c8327e09675b66d
KYapp <kukiyomenaisapp@gmail.com>
2023-02-26 22:50:17 +0900

    MultiplayEngineのガワ

---
+++ Assets/popoMultiplay/Runtime/MultiplayEngine.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+using System.Collections.Generic;
+using NativeWebSocket;
+using JuhaKurisu.PopoTools.ByteSerializer;
+
+namespace JuhaKurisu.PopoTools.Multiplay
+{
+    public class MultiplayEngine
+    {
+    }
+}
