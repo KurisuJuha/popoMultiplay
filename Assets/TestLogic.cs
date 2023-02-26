0051a29fe17c544d70b305a48ae0bc9e42c122f6
KYapp <kukiyomenaisapp@gmail.com>
2023-02-27 00:07:27 +0900

    testlogic

--- Assets/TestLogic.cs
+++ Assets/TestLogic.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
 using JuhaKurisu.PopoTools.Multiplay;
 
 public class TestLogic
 {
+    public Dictionary<ClientID, ulong> counter = new();
+    public int elapsedFrame;
+
     public void Tick(MultiplayClient[] clients)
     {
+        foreach (var client in clients)
+        {
+            if (client.input.spaceButton)
+                counter[client.id]++;
+        }
 
+        elapsedFrame++;
     }
 }
